3c73157b30a156756c6b5af3bc339f1e81aaa255
Maheswari <maheswari.rajendran31@gmail.com>
2023-06-29 19:53:04 +0530

    first commit

---
+++ codes/60809bd9-ce3d-4a51-8b92-f76e04903605.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+cout<<"hello world8";
+return 0;
+}
---
+++ routes/problems.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const Problem = require('../models/ProblemModels')
+const {
+  getProblems,
+  getSingleProblemById,
+  createProblemById,
+  createNewProblemById
+} = require('../controllers/problemControllers')
+const router = express.Router()
+
+// GET all workouts
+router.get('/getProblems', getProblems)
+
+// GET a single workout
+router.get('/getSingleProblemById/:id', getSingleProblemById);
+//router.get('/getProblemBySerialNo/:no', getProblemBySerialNo);
+
+// POST a new workout
+router.post('/createProblemById/run', createProblemById);
+
+// POST a new workout
+router.post('/createNewProblemById/run', createNewProblemById);
+/*
+router.post('/', async (req, res) => {
+    const {
+        serial_no,title,
+        description,
+        languages,
+        difficulty,
+        test_case
+    } = req.body
+  
+    try {
+      const problem = await Workout.create({  serial_no,title,
+        description,
+        languages,
+        difficulty,
+        test_cases
+    })
+      res.status(200).json(problem)
+    } catch (error) {
+      res.status(400).json({error: error.message})
+    }
+  })
+
+// DELETE a workout
+router.delete('/:id', (req, res) => {
+  res.json({mssg: 'DELETE a new problem'})
+})
+
+// UPDATE a workout
+router.patch('/:id', (req, res) => {
+  res.json({mssg: 'UPDATE a problem'})
+})*/
+
+module.exports = router
